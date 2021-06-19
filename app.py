4bca8438926775881b33e089353d05ae07741fde
Cibhi Baskar <cibhi.baskar@gmail.com>
2021-06-20 00:11:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 app.secret_key = 'performancedashboard'
 
 # Database connection details below
-app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Appu@05022000'
 app.config['MYSQL_DB'] = 'bank'
