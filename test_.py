baa4abc3ebfe6dd7d153924b0610495ca0e26c0e
Cibhi Baskar <cibhi.baskar@gmail.com>
2021-06-23 19:49:55 +0530

    Update test_.py

--- test_.py
+++ test_.py
@@ -14,7 +14,7 @@ class BasicTestCase(unittest.TestCase):
         
     def test_home(self):
         tester = app.test_client(self)
-        pages = ['/', 'dashboard', 'predict']
+        pages = ['/', 'dashboard']
         for page in pages:
             response = tester.get(page, content_type='html/text')
             self.assertEqual(response.status_code, 200)
